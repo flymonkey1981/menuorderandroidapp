811772cd3cd66dea8e28a95f33900a305120b90d
charles.chen <charles.chen@iress.com>
2019-01-26 20:46:29 +1100

    Initial commit

---
+++ app/src/main/java/com/example/charleschen/menu_order_android_app/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.charleschen.menu_order_android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.util.HttpUtils;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button loginButton = findViewById(R.id.button);
+        final EditText userName = findViewById(R.id.userName);
+        final EditText password = findViewById(R.id.password);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Clicked","button is clicked");
+
+                RequestParams rp = new RequestParams();
+                rp.add("userName", userName.getText().toString()); rp.add("password", password.getText().toString());
+
+                HttpUtils.post("/findByUserNameAndPassword", rp, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // If the response is JSONObject instead of expected JSONArray
+                        Log.d("asd", "---------------- this is response : " + response);
+                        try {
+                            JSONObject serverResp = new JSONObject(response.toString());
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                        // Pull out the first event on the public timeline
+
+                    }
+                });
+            }
+        });
+    }
+}
